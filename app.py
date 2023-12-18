50b234f94ca872d2799ac13341b40e83ed811680
Nivash_sharma <nivasmathu@gmail.com>
2023-12-18 12:06:02 +0530

    ,

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import streamlit as st
 Name=st.text_input("enter your name" )
 st.write("Hello",Name)
 age=st.number_input("enter the age" )
-st.write("your age is," age)
+st.write("your age is", age)
 
