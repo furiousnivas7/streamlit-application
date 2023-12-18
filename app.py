b988a140b258b3a2dcb6528372f73a23423a1921
Nivash_sharma <nivasmathu@gmail.com>
2023-12-18 17:17:01 +0530

    add placeholder

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
-st.title("pass or fail")
-Name=st.text_input("enter your name" )
+st.title("Pass or Fail")
+Name=st.text_input("enter your name" ,placeholder="enter the Name")
 st.write("Hello",Name)
-mark=st.number_input("enter the mark" )
+mark=st.number_input("enter the mark",placeholder="enter the number" )
 st.write("your mark1 is",mark)
-mark2=st.number_input("enter the second marks")
+mark2=st.number_input("enter the second marks",placeholder="enter the number")
 st.write("your mark2 is", mark2)
-mark3=st.number_input("enter the third marks")
+mark3=st.number_input("enter the third marks",placeholder="enter the number")
 st.write("your third marks is",mark3)
