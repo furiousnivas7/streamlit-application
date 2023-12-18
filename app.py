a6eb163dd41a1b66c077f12994694c773f9356e6
Nivash_sharma <nivasmathu@gmail.com>
2023-12-18 17:12:23 +0530

    add mark1,mark2

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+
+st.title("pass or fail")
 Name=st.text_input("enter your name" )
 st.write("Hello",Name)
-age=st.number_input("enter the age" )
-st.write("your age is", age)
-
+mark=st.number_input("enter the mark" )
+st.write("your mark1 is",mark)
+mark2=st.number_input("enter the second marks")
+st.write("your mark2 is", mark2)
+mark3=st.number_input("enter the third marks")
+st.write.("your third marks is",mark3)
