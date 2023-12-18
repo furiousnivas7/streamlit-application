8cccdcbd566ecbfbbabe9cf3b6933e846699b1b8
Nivash_sharma <nivasmathu@gmail.com>
2023-12-18 17:22:11 +0530

    add avgrage

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ mark=st.number_input("enter the mark",placeholder="enter the number" )
 mark2=st.number_input("enter the second marks",placeholder="enter the number")
 mark3=st.number_input("enter the third marks",placeholder="enter the number")
 tot=mark+mark2+mark3
+avg=tol/3
 
 st.write("Hello",Name)
 st.write("your mark1 is",mark)
 st.write("your mark2 is", mark2)
 st.write("your third marks is",mark3)
 st.write("your total is",tot)
+st.write("your avgrage is ",avg)
