9cebc2e035b149d6d2429b008f9b905c333f3513
Nivash_sharma <nivasmathu@gmail.com>
2023-12-18 17:20:12 +0530

    add total

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import streamlit as st
 
 st.title("Pass or Fail")
 Name=st.text_input("enter your name" ,placeholder="enter the Name")
-st.write("Hello",Name)
 mark=st.number_input("enter the mark",placeholder="enter the number" )
-st.write("your mark1 is",mark)
 mark2=st.number_input("enter the second marks",placeholder="enter the number")
-st.write("your mark2 is", mark2)
 mark3=st.number_input("enter the third marks",placeholder="enter the number")
+tot=mark+mark2+mark3
+
+st.write("Hello",Name)
+st.write("your mark1 is",mark)
+st.write("your mark2 is", mark2)
 st.write("your third marks is",mark3)
+st.write("your total is",tot)
