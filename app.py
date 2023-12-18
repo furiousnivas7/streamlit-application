28aced46b31806149f0448433519a62bc80ea19b
Nivash_sharma <nivasmathu@gmail.com>
2023-12-18 18:03:53 +0530

    making marks as integer

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ Name=st.text_input("enter your name" ,placeholder="enter the Name")
 mark=st.number_input("enter the mark",placeholder="enter the number", value=None )
 mark2=st.number_input("enter the second marks",placeholder="enter the number",value=None)
 mark3=st.number_input("enter the third marks",placeholder="enter the number",value=None)
-#tot=mark+mark2+mark3
+tot=int(mark)+int(mark2)+int(mark3)
 #avg=tot/3
 
 #if avg>45:
@@ -20,5 +20,5 @@ st.write("Hello",Name)
 st.write("your mark1 is",mark)
 st.write("your mark2 is", mark2)
 st.write("your third marks is",mark3)
-#st.write("your total is",tot)
+st.write("your total is",tot)
 #st.write("your avgrage is ",avg)
