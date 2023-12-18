4edb2b13a53eef73bd0c2318aeb8cce786e91d3b
Nivash_sharma <nivasmathu@gmail.com>
2023-12-18 17:33:30 +0530

    remove some commas

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ tot=mark+mark2+mark3
 avg=tot/3
 
 if avg>45:
-    st.write("your are",Pass,"the exam")
+    st.write("your are Pass the exam")
 else:
-    st.write("your are",fail,"the exam")
+    st.write("your are,fail the exam")
 
 st.write("Hello",Name)
 st.write("your mark1 is",mark)
